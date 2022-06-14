77e3cb3fa163b2c222a044a9f05b7f3692556d21
prabhosle <102647938+prabhosle@users.noreply.github.com>
2022-06-14 19:05:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var a = document.getElementsByClassName('drum')
+
+     
+
+
+
+    a.addEventListener("click",function(){
+   this.style.color="white"  }
+    )
+
+
+    function play1(){
+        var a = new Audio('sounds/snare.mp3')
+        a.play()
+    }
+    function play2(){
+        var a = new Audio('sounds/crash.mp3')
+        a.play()
+    }
+    function play3(){
+        var a = new Audio('sounds/kick-bass.mp3')
+        a.play()
+    }
+    function play4(){
+        var a = new Audio('sounds/tom-1.mp3')
+        a.play()
+    }
+    function play5(){
+        var a = new Audio('sounds/tom-2.mp3')
+        a.play()
+    }
+    function play6(){
+        var a = new Audio('sounds/tom-3.mp3')
+        a.play()
+    }
+    function play7(){
+        var a = new Audio('sounds/tom-4.mp3')
+        a.play()
+    }
